9e1a5e5535095e4889e12304acff02518b5ebd5a
Alexsandro Zanella Arruda <65736062+AlexsandroZanella2@users.noreply.github.com>
2020-08-11 07:45:26 -0300

    Inicial

---
+++ Unit1.dfm
@@ -0,0 +1,135 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Scrapper S'#233'ries RedeCanais'
+  ClientHeight = 498
+  ClientWidth = 893
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object PageControl1: TPageControl
+    Left = 0
+    Top = 80
+    Width = 893
+    Height = 418
+    ActivePage = TabSheet2
+    Align = alBottom
+    TabOrder = 0
+    object TabSheet1: TTabSheet
+      Caption = 'S'#233'ries'
+      ExplicitHeight = 350
+      object Memo1: TMemo
+        Left = 0
+        Top = 0
+        Width = 885
+        Height = 390
+        Align = alClient
+        TabOrder = 0
+        ExplicitLeft = 32
+        ExplicitTop = 88
+        ExplicitWidth = 185
+        ExplicitHeight = 89
+      end
+    end
+    object TabSheet2: TTabSheet
+      Caption = 'V'#237'deos'
+      ImageIndex = 1
+      ExplicitHeight = 350
+      object Memo2: TMemo
+        Left = 0
+        Top = 0
+        Width = 885
+        Height = 390
+        Align = alClient
+        TabOrder = 0
+        ExplicitLeft = 32
+        ExplicitTop = 88
+        ExplicitWidth = 185
+        ExplicitHeight = 89
+      end
+    end
+    object TabSheet3: TTabSheet
+      Caption = 'Links'
+      ImageIndex = 2
+      ExplicitLeft = 0
+      ExplicitHeight = 350
+      object Memo3: TMemo
+        Left = 0
+        Top = 0
+        Width = 885
+        Height = 390
+        Align = alClient
+        TabOrder = 0
+        ExplicitLeft = 32
+        ExplicitTop = 88
+        ExplicitWidth = 185
+        ExplicitHeight = 89
+      end
+    end
+    object TabSheet4: TTabSheet
+      Caption = 'Resultado'
+      ImageIndex = 3
+      ExplicitLeft = 8
+      object Memo4: TMemo
+        Left = 0
+        Top = 0
+        Width = 885
+        Height = 390
+        Align = alClient
+        TabOrder = 0
+        ExplicitLeft = 32
+        ExplicitTop = 88
+        ExplicitWidth = 185
+        ExplicitHeight = 89
+      end
+    end
+  end
+  object Edit1: TEdit
+    Left = 8
+    Top = 8
+    Width = 321
+    Height = 21
+    TabOrder = 1
+    Text = 'https://redecanais.bz/browse-letra-a-series-videos-1-date.html'
+  end
+  object Button1: TButton
+    Left = 335
+    Top = 8
+    Width = 98
+    Height = 25
+    Caption = 'Buscar S'#233'ries'
+    TabOrder = 2
+    OnClick = Button1Click
+  end
+  object Button2: TButton
+    Left = 8
+    Top = 35
+    Width = 113
+    Height = 39
+    Caption = 'Buscar V'#237'deos'
+    TabOrder = 3
+    OnClick = Button2Click
+  end
+  object Button3: TButton
+    Left = 127
+    Top = 35
+    Width = 98
+    Height = 39
+    Caption = 'Frames'
+    TabOrder = 4
+  end
+  object Button4: TButton
+    Left = 231
+    Top = 35
+    Width = 202
+    Height = 39
+    Caption = 'Gerar'
+    TabOrder = 5
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,132 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ComCtrls, IdBaseComponent, IdComponent, IdTCPConnection, IdTCPClient,
+  IdHTTP, IdIOHandler, IdIOHandlerSocket, IdIOHandlerStack, IdSSL, IdSSLOpenSSL,
+  System.Net.URLClient, System.Net.HttpClient, System.Net.HttpClientComponent,
+  WinINet;
+
+type
+  TForm1 = class(TForm)
+    PageControl1: TPageControl;
+    TabSheet1: TTabSheet;
+    TabSheet2: TTabSheet;
+    TabSheet3: TTabSheet;
+    TabSheet4: TTabSheet;
+    Edit1: TEdit;
+    Button1: TButton;
+    Memo1: TMemo;
+    Memo2: TMemo;
+    Memo3: TMemo;
+    Memo4: TMemo;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+function GetURL(const AURL: string): string;
+var
+  HttpClient: THttpClient;
+  HttpResponse: IHttpResponse;
+begin
+  HttpClient := THttpClient.Create;
+  try
+    HttpResponse := HttpClient.Get(AURL);
+    Result := HttpResponse.ContentAsString();
+  finally
+    HttpClient.Free;
+  end;
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+retorno, lista:tstringlist;
+i,ii,g,j:integer;
+identificador,link:string;
+begin
+  //
+  retorno := TStringList.Create;
+  lista   := TStringList.Create;
+    retorno.Text := GetURL(edit1.Text);
+    identificador := '<li><a href="/browse-a';
+
+    while retorno.Text.Contains(identificador) do begin
+      retorno.Text := copy(retorno.Text, pos(identificador,retorno.Text));
+      link := copy(retorno.Text, pos('/',retorno.Text));
+      link := copy(link,1,pos('"',link)-1);
+      lista.Add('https://redecanais.bz'+link);
+      retorno.Text := copy(retorno.Text,5);
+    end;
+
+
+  memo1.Lines.Text := lista.Text;
+  lista.Destroy;
+  retorno.Destroy;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+var
+retorno,lista, temp:TStringList;
+i,ii,g,j:integer;
+identificador,link, temporada, episodeo,opcao, titleSerie:string;
+begin
+    retorno := TStringList.Create;
+    lista   := TStringList.Create;
+    temp    := TStringList.Create;
+
+    identificador := '/musicvideo.php?';
+
+    for I := 0 to memo1.Lines.Count -1 do begin
+      retorno.Text := GetURL(memo1.Lines[i]);
+      retorno.Text := Copy(retorno.Text, pos('<title>',retorno.Text)+7);
+      titleSerie := Copy(retorno.Text, 1,pos('</title>',retorno.Text));
+      while retorno.Text.Contains('/musicvideo.php?vid=') do begin
+
+        if pos('Temporada</strong>',retorno.Text) < pos('/musicvideo.php?vid=',retorno.Text) then begin
+          retorno.Text := copy(retorno.Text, pos('Temporada</strong>',retorno.Text)-20);
+          retorno.Text := copy(retorno.Text, pos('<strong>',retorno.Text)+8);
+          temporada := copy(retorno.Text,1,pos('&',retorno.Text)-1);
+          retorno.text := copy(retorno.Text,pos('</strong>',retorno.Text));
+        end else begin
+          retorno.Text := copy(retorno.Text, pos('<strong>Epis&oacute;dio ', retorno.Text));
+          retorno.Text := copy(retorno.Text, length('<strong>Epis&oacute;dio ')+1);
+          episodeo := copy(retorno.Text, 1,pos(' ',retorno.Text)-1);
+          retorno.Text := copy(retorno.Text, pos('/musicvideo.php?vid=',retorno.Text));
+          if pos('dublado',retorno.Text) < pos('Legendado',retorno.Text) then begin
+             opcao := 'Dublado';
+          end else begin
+            opcao := 'Legendado'
+          end;
+          link := copy(retorno.Text, 1,pos('"',retorno.Text)-1);
+          link := 'https://redecanais.bz'+link;
+          lista.Add(link+'@T'+temporada+'E'+episodeo+' '+opcao+'@'+titleSerie);
+          retorno.Text := copy(retorno.Text, pos(opcao,retorno.Text));
+
+        end;
+      end;
+    end;
+
+
+    memo2.Lines.Text := lista.Text;
+    temp.Destroy;
+    lista.Destroy;
+    retorno.Destroy;
+end;
+
+end.
